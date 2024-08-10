37a78fe9a469bf3217d1e4936f66b33cbec4e8c3
Jonnas Costa <jonn567@gmail.com>
2024-08-10 11:37:13 +0100

    Update terminal keybindings

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -4,8 +4,8 @@ return {
     version = '*',
     config = true,
     opts = {
-      vim.keymap.set('n', '<leader>th', '<cmd>ToggleTerm direction=horizontal<CR>', { noremap = true, silent = true, desc = 'Toggle Terminal Horizontal' }),
-      vim.keymap.set('n', '<leader>tv', '<cmd>ToggleTerm direction=vertical<CR>', { noremap = true, silent = true, desc = 'Toggle Terminal Vertical' }),
+      vim.keymap.set('n', '<A-h>', '<cmd>ToggleTerm direction=horizontal<CR>', { noremap = true, silent = true, desc = 'Toggle Terminal Horizontal' }),
+      vim.keymap.set('n', '<A-v>', '<cmd>ToggleTerm direction=vertical<CR>', { noremap = true, silent = true, desc = 'Toggle Terminal Vertical' }),
     },
   },
 }
